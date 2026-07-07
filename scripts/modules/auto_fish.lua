8e50b2c24458ada7f569f58eba533a4788b302e2
qwertys50 <kchess5gorsk2@gmail.com>
2026-07-07 16:54:06 +0300

    Create auto_fish.lua

---
+++ scripts/modules/auto_fish.lua
@@ -0,0 +1,81 @@
+local plr = game.Players.LocalPlayer
+
+local function StartFishing()
+    game:GetService("ReplicatedStorage").Network.Instancing_FireCustomFromClient:FireServer(
+        "Fishing",
+        "RequestCast",
+        Vector3.new(1126.1120605469, 75.914115905762, -3445.1215820312)
+    )
+end
+
+local function PodFishing()
+    game:GetService("ReplicatedStorage").Network.Instancing_FireCustomFromClient:FireServer(
+        "Fishing",
+        "RequestReel",
+        nil
+    )
+end
+
+local function ClickFishing()
+    game:GetService("ReplicatedStorage").Network.Instancing_InvokeCustomFromClient:InvokeServer(
+        "Fishing",
+        "Clicked"
+    )
+end
+
+local started = false
+local started_fish = false
+
+game:GetService("ReplicatedStorage").Network.Instancing_FireCustomFromServer.OnClientEvent:Connect(function(a, b, c)
+    if typeof(c) ~= "string" then if c ~= plr then return end end
+    if a ~= "Fishing" then return end
+
+    if b == "Cast" then
+        started_fish = false
+    end
+
+    if b == "Hook" then
+        
+        task.wait(0.5)
+        PodFishing()
+    end
+
+    if b == "StartAttemptCatch" then
+        started = true
+    end
+
+    if b == "RemoveCast" then
+        started_fish = true
+    end
+
+    if b == "FishingSuccess" then
+        
+        started = false
+        started_fish = true
+        
+    end
+
+end)
+
+task.spawn(function()
+    
+    while task.wait(0.1) do
+        
+        if started then
+            ClickFishing()
+        end
+
+        if started_fish then
+            StartFishing()
+        end
+    end
+end)
+
+return {
+    start = (function()
+        started_fish = true
+    end),
+    stop = (function()
+        started_fish = false
+    end)
+}
